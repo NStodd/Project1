da36e5af19d2e45f6679360f7857e896467d0953
Nate Stodd <nathan.stoddard@gmail.com>
2022-10-22 17:14:34 -0400

    preliminary work #1. -nes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const quote_url = "https://api.goprogram.ai/inspiration"
+// Constants and variables
+const QUOTE_URL = "https://api.goprogram.ai/inspiration"
+const IMG_URL = "https://pixabay.com/api/?"
 
 const quote_req = $.ajax(quote_url)
 
 console.log(quote_req)
 
-const img_url = "https://pixabay.com/api/?"
+// Cached Elements
+
+
 const img_api_key ="30787297-8988965d9b3903eaa7f0d16eb"
+
+// Event Listeners
 
 function buildImgUrl(base, api, term) {
     const url = base + "key=" + api + "&q=" + term
@@ -15,3 +21,6 @@ function buildImgUrl(base, api, term) {
 const img_req = $.ajax(buildImgUrl(img_url, img_api_key, "nature"))
 
 console.log(img_req)
+
+// Functions
+
