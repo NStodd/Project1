5b693a8078809ef96b2c7891503cdd7740a0afce
Nate Stodd <nathan.stoddard@gmail.com>
2022-10-24 22:59:52 -0400

    got the image object working to return stuff -nes

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 // Constants and variables
 const QUOTE_URL = "https://api.goprogram.ai/inspiration"
 const IMG_URL = "https://pixabay.com/api/?"
-
-const quote_req = $.ajax(quote_url)
-
-console.log(quote_req)
+const IMG_API_KEY = "30787297-8988965d9b3903eaa7f0d16eb"
 
 // Cached Elements
 
+// EVENT LISTENERS
 
-const img_api_key ="30787297-8988965d9b3903eaa7f0d16eb"
+// FUNCTIONS
 
-// Event Listeners
+// ------ getJSON function is hoisted function that takes a url and returns a promise
+function getJSON(url){
+    return fetch(url).then((response) => response.json())
+}
 
+// ------ buildImgUrl: function to take in parameters and build a url to use
+//                              in the image fetch command.
 function buildImgUrl(base, api, term) {
     const url = base + "key=" + api + "&q=" + term
     return url
 }
 
-const img_req = $.ajax(buildImgUrl(img_url, img_api_key, "nature"))
 
+// EXECUTING CODE
+
+// call the image api request using getJSON and buildImgUrl
+const img_req = getJSON(buildImgUrl(IMG_URL,IMG_API_KEY,"nature"))
 console.log(img_req)
+img_req.then(
+    function (img_obj) {
+        console.log(img_obj.hits)
+    }
+)
 
-// Functions
+// call the image api request using $.ajax
+const ajax_img_req = $.ajax(buildImgUrl(IMG_URL,IMG_API_KEY,"nature"))
+console.log(ajax_img_req)
 
+// Deprecated
+
+// initial request, using ajax rather than 'fetch' >>> see getJSON function
+// const quote_req = $.ajax(quote_url)
+// console.log(quote_req)
